5a9dc901a1495628c461d5483418869a62e6ec26
shipra-trivedi <49556755+shipra-trivedi@users.noreply.github.com>
2021-08-12 10:28:06 +0530

    Update readFilecode.groovy

--- vars/readFilecode.groovy
+++ vars/readFilecode.groovy
@@ -1,1 +1,16 @@
+#!/usr/bin/env groovy
 
+def call(String str ) {
+	
+	def filePath = readFile "${str}"                   
+	def lines = filePath.readLines()
+	 def linesbyline = filePath.readLines() 
+      
+	for (line in linesbyline) {                                            
+                      
+                        println "$line"
+                        }  
+  
+ 
+   
+}
