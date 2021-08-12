d7ce82af34e4861ced70e9be7e6186324f223977
shipra-trivedi <49556755+shipra-trivedi@users.noreply.github.com>
2021-08-12 10:47:14 +0530

    Create filedemo.groovy

---
+++ src/com/cleverbuilder/filedemo.groovy
@@ -0,0 +1,10 @@
+import java.io.File 
+class Example { 
+   static void main(String[] args) { 
+      new File("Hello.csv").eachLine {  
+         line -> println "$line"; 
+
+    
+      } 
+   } 
+}
